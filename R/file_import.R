1154a5afec38415b002171595f4c72941ca8793a
jmackie4 <jordan.mackie48@googlemail.com>
2016-06-10 14:31:31 +0100

    dplyr now exports the select_helpers.

--- R/file_import.R
+++ R/file_import.R
@@ -121,8 +121,8 @@ import_ride.fit <- function(file_path, raw = FALSE, make_laps = TRUE, ...) {
   }
 
   # Enhanced columns aren't parsed correctly:
-  # https://www.thisisant.com/forum/viewthread/4561
-  out <- dplyr::select_(out, quote(-contains("enhanced")))
+  # See also: https://www.thisisant.com/forum/viewthread/4561
+  out <- dplyr::select_(out, quote(-dplyr::contains("enhanced")))
 
   # Create lap column.
   if (make_laps) {
