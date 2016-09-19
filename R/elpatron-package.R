526b38fdba48fba10a1cf1d114a70b5477b7e3e5
jmackie4 <jordan.mackie48@googlemail.com>
2016-09-19 11:50:09 +0100

    Missing monofont corrected.

--- R/elpatron-package.R
+++ R/elpatron-package.R
@@ -5,25 +5,25 @@
 #'
 #' \code{elpatron} supercedes the \code{cycleRtools} package, if only for having
 #' a cooler name. Generally speaking, this package attempts to do much less than
-#' cycleRtools. But what it does, it does much better. File reading functions
-#' are now much more efficient; raw data is left largely untouched by default;
-#' and certain assumptions that had to be made for more \emph{involved} routines
-#' are now left to the user to explicitly resolve. Generally speaking, the best
-#' bits of cycleRtools have been skimmed and improved.
+#' \code{cycleRtools}. But what it does, it does much better. File reading
+#' functions are now much more efficient; raw data is left largely untouched by
+#' default; and certain assumptions that had to be made for more \emph{involved}
+#' routines are now left to the user to explicitly resolve. Generally speaking,
+#' the best bits of cycleRtools have been skimmed and improved.
 #'
 #' This package came with the realisation that the advantage of using R is not
 #' convenience \emph{per se}, but rather its attendant freedom and flexibility.
 #' Hence, rather than provide menial functions for common analysis routines,
 #' this package simply aims to be \emph{facilitative}.
 #'
-#' The package is also intended to work seamlessly with those of Hadley Wickham
-#' \emph{et al.} (notably \code{dplyr} and \code{purrr}).
+#' The package is also intended to work seamlessly with the \code{tidyverse}.
 #'
 #' @useDynLib elpatron
 #' @importFrom Rcpp sourceCpp
+#' @importFrom fitdc read_fit unpack
 #' @importFrom dplyr tbl_df
 #' @importFrom magrittr %>%
-#' @importFrom purrr map map_lgl map_dbl map_df
+#' @importFrom purrr map map_lgl map_chr map_dbl map_df
 #' @importFrom lazyeval lazy_dots
 #'
 #' @import stats
@@ -42,10 +42,4 @@ NULL
 
 # ------------------------------------------
 # Notes to self:
-#
-# + pugixml needs to be in header-only mode
-# + I've included all files from the FIT SDK cpp dir, but this is
-#   probably overkill. I need some way to figure out what is actually
-#   being used.
-# + CMD check doesn't like *.hpp file extensions, use *.h (e.g with
-#   a project-wide find && replace).
+#   + pugixml needs to be in header-only mode
